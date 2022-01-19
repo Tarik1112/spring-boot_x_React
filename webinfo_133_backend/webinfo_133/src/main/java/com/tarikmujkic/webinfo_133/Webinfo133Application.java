e71695c81646e9452b45f76fb97d52228fbbb915
Tarik Mujkic <tarik.mujkic@commercecare.de>
2022-01-19 12:17:33 +0100

    Initialisation

---
+++ webinfo_133_backend/webinfo_133/src/main/java/com/tarikmujkic/webinfo_133/Webinfo133Application.java
@@ -0,0 +1,13 @@
+package com.tarikmujkic.webinfo_133;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webinfo133Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webinfo133Application.class, args);
+	}
+
+}
---
+++ webinfo_133_backend/webinfo_133/src/test/java/com/tarikmujkic/webinfo_133/Webinfo133ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarikmujkic.webinfo_133;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webinfo133ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
